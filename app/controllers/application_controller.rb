c4db63a75a60af65a693e7c5c38d9fa1dd1f8f63
Penny Bentley <penny.bentley@cox.net>
2019-10-17 02:51:34 +0000

    final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
 end
+
+class Kitchen (dish, color='white')
+    attr_accessor :dish, :color
+    
+    def initialize (dish, color)
+        @dish = dish
+        @color = color
+    end
+    
+end
+
+kitchen = Kitchen.new('cup', 'blue')
+
+puts kitchen
+puts kitchen.dish
+puts kitchen.color
